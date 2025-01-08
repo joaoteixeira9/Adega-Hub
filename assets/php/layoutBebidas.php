40ce14aeef146028427456f206c890d9c2b96917
joaoteixeira_ <jp2887860@gmail.com>
2025-01-08 01:42:27 -0300

    Updates

--- assets/php/layoutBebidas.php
+++ assets/php/layoutBebidas.php
@@ -1,16 +1,15 @@
 <section class="content">
         <?php
-        $sql = "select * from bebidas";
+        $sql = "SELECT * FROM bebidas ORDER BY RAND()";
         $res = mysqli_query($conexao, $sql);
         echo "<ol class='bebidas'>";
             while ($l = mysqli_fetch_assoc($res)) {
                 echo "<li class='" . $l['rotulo'] . "'>";
                     echo $l['nome'];
-                    echo "<img src='./img/" . $l['nome'] .".png' alt='imagens-bebidas'>";
+                    echo "<img src='./img/" . $l['nome'] .".png' alt=''>";
                 echo "</li>";
             }
         echo "</ol>";
         mysqli_close($conexao);
         ?>
     </section>
-    <img src="../../assets/img/ballantines.png" alt="">
--- index.php
+++ index.php
@@ -1,43 +1,6 @@
 <?php 
     include "./assets/php/conexao.php"; 
     include "./assets/php/header.php";
-
     include "./assets/php/layoutBebidas.php";
-
+    include "./assets/php/footer.php";
 ?>
-    <footer>
-        <div class="infoAdega">
-            <img src="./assets/img/fachada-adega.jpg" class="img-thumbnail img-fluid" alt="">
-            <div class="contatos">
-                <h6>Rua das Amoras, nº 245, Bairro Jardim Encantado, Vila Nova, São Paulo, CEP 12345-678</h6>
-                <h6>(19) 98765-4321</h6>
-                <h6>adm.adegahub@gmail.com</h6>
-                <h6>Segunda a Sexta: 18h - 22h</h6>
-                <h6>Sábado e Domingo: 18h - 00h</h6>
-                <h6>Venda de bebidas alcoólicas proibida para menores de 18 anos.</h6>
-            </div>
-        </div>
-        <div class="outros">
-            <div class="formaPag">
-                <h6>Formas de pagamento</h6>
-                <img src="./assets/img/mastercard.png" alt="">
-                <img src="./assets/img/visa.png" alt="">
-                <img src="./assets/img/elo.png" alt="">
-                <img src="./assets/img/pix.png" alt="">
-                <img src="./assets/img/dinners-club.png" alt="">
-                <img src="./assets/img/hipercard.png" alt="">
-            </div>
-            <div class="redesSociais">
-                <h6>Siga-nós</h6>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-threads"></i></a>
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-whatsapp"></i></a>
-                <a href=""><i class="bi bi-tiktok"></i></a>
-                <a href=""><i class="bi bi-twitter-x"></i></a>
-            </div>
-        </div>
-    </footer>
-    <script src="./js/main.js"></script>
-</body>
-</html>
