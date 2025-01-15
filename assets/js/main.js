a92b41bef0126e21cfdbb65526d9a354bd4c3131
joaoteixeira_ <jp2887860@gmail.com>
2025-01-15 15:36:21 -0300

    Atualizações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleciona os elementos pelos IDs específicos
     const cardCombos = document.getElementById('cardCombos');
     const cardQuemSomos = document.getElementById('cardQuemSomos');
     const cardProdutos = document.getElementById('cardProdutos');
     const principal = document.getElementById('principal');
     const video = document.getElementById('background-video');
 
-    // Função de hover para cardCombos
     cardCombos.addEventListener('mouseenter', function() {
         cardCombos.classList.add('cardComboHovered');
         principal.classList.add('principalCombo');
-        // Reduz a opacidade dos outros cards
         cardQuemSomos.style.opacity = '0';
         cardProdutos.style.opacity = '0';
     });
     cardCombos.addEventListener('mouseleave', function() {
         cardCombos.classList.remove('cardComboHovered');
         principal.classList.remove('principalCombo');
-        // Restaura a opacidade dos outros cards
         cardQuemSomos.style.opacity = '1';
         cardProdutos.style.opacity = '1';
     });
 
-    // Função de hover para cardQuemSomos
     cardQuemSomos.addEventListener('mouseenter', function() {
         cardQuemSomos.classList.add('cardQuemSomosHovered');
         principal.classList.add('principalQuemSomos');
@@ -33,18 +28,15 @@ document.addEventListener("DOMContentLoaded", function() {
     cardQuemSomos.addEventListener('mouseleave', function() {
         cardQuemSomos.classList.remove('cardQuemSomosHovered');
         principal.classList.remove('principalQuemSomos');
-        // Restaura a opacidade dos outros cards
         cardCombos.style.opacity = '1';
         cardProdutos.style.opacity = '1';
     });
 
-    // Função de hover para cardProdutos
     cardProdutos.addEventListener('mouseenter', function() {
         cardProdutos.classList.add('cardProdutosHovered');
         principal.classList.add('principalProdutos');
         video.src = './img/produtos.mp4';
         video.play();
-        // Reduz a opacidade dos outros cards
         cardCombos.style.opacity = '0';
         cardQuemSomos.style.opacity = '0';
     });
@@ -52,7 +44,6 @@ document.addEventListener("DOMContentLoaded", function() {
         cardProdutos.classList.remove('cardProdutosHovered');
         principal.classList.remove('principalProdutos');
         video.src = '';
-        // Restaura a opacidade dos outros cards
         cardCombos.style.opacity = '1';
         cardQuemSomos.style.opacity = '1';
     });
