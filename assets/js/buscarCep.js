25aa96a8c43c31f5f82e2382e697e01842c5cee0
joaoteixeira_ <jp2887860@gmail.com>
2025-03-04 18:48:54 -0300

    API de cep funcionando

--- assets/js/buscarCep.js
+++ assets/js/buscarCep.js
@@ -1,3 +1,34 @@
-function BuscarCep(){
+document.querySelector(".endereco").style.display = "none";
+function BuscarCep(cep){
     let url = `https://cep.awesomeapi.com.br/json/${cep}`;
+    fetch(url)
+    .then (resultado => {
+        return resultado.json();
+    })
+    .then (dados => {
+        return MostrarDados(dados.address, dados.district, dados.city, dados.state);
+    })
 }
+
+function MostrarDados(rua, bairro, cidade, estado){
+    document.getElementById("rua").value = rua;
+    document.getElementById("bairro").value = bairro;
+    document.getElementById("cidade").value = cidade;
+    document.getElementById("estado").value = estado;
+}
+
+const botaoCep = document.getElementById("botaoCep");
+botaoCep.addEventListener("click", function(event){
+    event.preventDefault();
+    let cep = document.getElementById("cep").value;
+    BuscarCep(cep);
+    document.querySelector(".endereco").style.display = "block";
+    document.querySelector(".info-cep").style.display = "none";
+})
+
+const mostrarBotaoCep = document.getElementById("mostrarBotaoCep");
+mostrarBotaoCep.addEventListener("click", function(event){
+    event.preventDefault();
+    document.querySelector(".endereco").style.display = "none";
+    document.querySelector(".info-cep").style.display = "block";
+})
--- assets/src/finalizarCompras/index.php
+++ assets/src/finalizarCompras/index.php
@@ -5,7 +5,7 @@
 <div class="enderecoEntrega mt-5">
     <h1 class="text-center mb-4">Confirme seu endereço</h1>
     <div class="text-center">
-        <form class="w-75 bg-light p-4 mx-auto">
+        <div class="info-cep w-75 bg-light p-4 mx-auto">
             <div class="mb-3">
                 <div class="form-group">
                     <label for="cep" class="form-label">Informe seu CEP</label>
@@ -15,10 +15,30 @@
                     </div>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary">Buscar Endereço</button>
-        </form>
+            <button type="submit" class="btn btn-primary" id="botaoCep">Buscar endereço</button>
+        </div>
+        
+        <div class="endereco text-start">
+            <div class="d-flex justify-content-center">
+                <div class="mt-3 mb-5 d-grid w-50 border p-3 rounded shadow">
+                    <label for="rua" class="form-label">Rua</label>
+                    <input type="text" class="form-control" name="rua" id="rua">
+                    <label for="numero" class="form-label">N°</label>
+                    <input type="text" class="form-control" name="numero" id="numero">
+                    <label for="bairro" class="form-label">Bairro</label>
+                    <input type="text" class="form-control" name="bairro" id="bairro">
+                    <label for="cidade" class="form-label">Cidade</label>
+                    <input type="text" class="form-control" name="cidade" id="cidade">
+                    <label for="estado" class="form-label">Estado</label>
+                    <input type="text" class="form-control" name="estado" id="estado">
+                    <button type="submit" class="mt-2 btn btn-primary" id="mostrarBotaoCep">Outro endereço</button>
+                    <button type="submit" class="mt-2 btn btn-success" id="botaoConfirmarCep">Confirmar endereço</button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+<script src="/Adega-Hub/assets/js/buscarCep.js"></script>
 <?php
     include "../../includes/footer.php";
 ?>
